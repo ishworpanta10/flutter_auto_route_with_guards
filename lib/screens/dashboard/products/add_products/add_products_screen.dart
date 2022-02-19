cbc468018fd891565a7a7cdbce2b8c9afa0df3dc
ishworpanta10 <ishworpanta10@gmail.com>
2022-02-19 10:26:41 +0545

    setup project screens files

---
+++ lib/screens/dashboard/products/add_products/add_products_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AddProductsScreen extends StatelessWidget {
+  const AddProductsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Add Product Screen'),
+      ),
+    );
+  }
+}
